34ab2107664b9c64a59a1506c4e9f44adb11c09c
Samuel Akinniranye <sakinnir@purdue.edu>
2023-03-08 19:08:27 -0500

    Login
    Login Storyboard and IBOutlets

---
+++ clean_cuts/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  clean_cuts
+//
+//  Created by Samuel Akinniranye on 3/8/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var Username: UITextField!
+    
+    @IBOutlet var Password: UITextField!
+    
+    @IBOutlet var Login: UIButton!
+    
+    @IBOutlet var Signup: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
