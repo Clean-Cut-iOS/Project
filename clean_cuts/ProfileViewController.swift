655746944c4c271414f9c1028e0ec7e05f214e3c
Bsmit4315 <89875684+Bsmit4315@users.noreply.github.com>
2023-03-10 09:29:24 -0500

    Profile Storyboard
    collection view and other stuff

---
+++ clean_cuts/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  clean_cuts
+//
+//  Created by Bryson Smith on 3/8/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
