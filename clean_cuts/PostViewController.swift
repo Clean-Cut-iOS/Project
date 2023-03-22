2bb34bdcfcc50173bdcba62e0811fecefa83ffb3
IrfanHussain2145 <ishussai@purdue.edu>
2023-03-22 18:45:40 -0400

    added stuff
    lsdjnfjdsnfjdsf

---
+++ clean_cuts/PostViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PostViewController.swift
+//  clean_cuts
+//
+//  Created by Irfan Hussain on 3/22/23.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    
+    @IBOutlet weak var username: UILabel!
+    
+    
+    @IBOutlet weak var imagecut: UIImageView!
+    
+    
+    @IBOutlet weak var barbertag: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
